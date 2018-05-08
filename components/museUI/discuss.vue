2f267976518a842717b6d2de1ba04749cd092b6a
Lee <jack2244057555@163.com>
2018-05-08 15:06:47 +0800

    4.8.1

--- components/museUI/discuss.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div id="discussions-root"><div data-reactroot="" class="subject-forum-topics"><h2><!-- react-text: 3 -->讨论(<!-- /react-text --><!-- react-text: 4 -->495<!-- /react-text --><!-- react-text: 5 -->)<!-- /react-text --></h2><ul class="list"><li><a href="/movie/discussion/615043167"><h3>我不是针对谁，我是说在座的打四星及以上的都是煞笔！</h3><div class="info"><!-- react-text: 11 -->36<!-- /react-text --><!-- react-text: 12 -->回应<!-- /react-text --></div></a></li><li><a href="/movie/discussion/615048757"><h3>没去电影院看完了</h3><div class="info"><!-- react-text: 17 -->1<!-- /react-text --><!-- react-text: 18 -->回应<!-- /react-text --></div></a></li><li><a href="/movie/discussion/615042171"><h3>笑过了，你们却说不好</h3><div class="info"><!-- react-text: 23 -->61<!-- /react-text --><!-- react-text: 24 -->回应<!-- /react-text --></div></a></li><li><a href="/movie/discussion/615044557"><h3>还是豆瓣聪明人多，B站的脑残不要太多</h3><div class="info"><!-- react-text: 29 -->17<!-- /react-text --><!-- react-text: 30 -->回应<!-- /react-text --></div></a></li><li><a href="/movie/discussion/615033623"><h3>全程尬到底，能打负分吗？</h3><div class="info"><!-- react-text: 35 -->153<!-- /react-text --><!-- react-text: 36 -->回应<!-- /react-text --></div></a></li><li class="go-list"><a href="/movie/subject/27038183/discussions">查看全部讨论</a></li></ul></div></div>
-</template>
-
-<script>
-</script>
-
-<style>
-</style>
